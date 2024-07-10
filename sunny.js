9f7317e724e54b51df63eac1f139afdd5d90ad93
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-10 21:12:02 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,67 @@
+// // immediately invoked function expression
+// // IIFE
+
+// // KISI B FUNCT KO EXECUTE KRAANY K LYE AP USLK BD SIMPLY BRACES LGAO R POORE FUNCTION LO B BRACES M DALO TO B WO EXECUTE HOJAEGA
+
+// (function one(){
+//      console.log(`hellllo`);
+//  })();
+
+
+
+
+
+
+//  // use semicolon to separate two iife otherwise errors will occur
+
+
+
+
+//   (  function one(){
+//         console.log(`hellllo`);
+//     }
+// )();
+
+// (
+//     ()=>{
+//         console.log(`hello world this is sanaiyla`);
+//     }
+// )()
+
+(
+    //     function one(){
+    //         console.log(`hello guys`);
+    //     }
+    // )();
+    // (
+    //      sunny = function(){
+    //         console.log(`helllo bci`);
+    //     }
+    // )();
+    // (
+    //     two = ()=>{
+    //         console.log(`helllo third vway`);
+    //     }
+    // )();
+    
+
+// (
+//     function two(){
+//         console.log(`helllo`);
+//     }
+// )();
+// (
+    (name1)=>{
+        console.log(`hello 2 , its ${name1}`);
+    }
+)(`sanaiyla`);
+(
+    ()=>{
+        console.log(`hellllo`);
+    }
+)();
+(
+    function(){
+        console.log(`hhhhhhh`);
+    }
+)()
